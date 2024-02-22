4e51cd6c15534d7c7a156781c11751582cd17f60
Samuel <samuel.manukonda.ext@yunextraffic.com>
2024-02-22 15:29:58 +0530

    refracting code

--- arrays/SingleNumber.java
+++ arrays/SingleNumber.java
@@ -1,19 +1,44 @@
 package arrays;
 
 //https://leetcode.com/problems/single-number/
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//
+//        You must implement a solution with a linear runtime complexity and use only constant extra space.
+//
+//
+//
+//        Example 1:
+//
+//        Input: nums = [2,2,1]
+//        Output: 1
+//        Example 2:
+//
+//        Input: nums = [4,1,2,1,2]
+//        Output: 4
+//        Example 3:
+//
+//        Input: nums = [1]
+//        Output: 1
+//
+//
+//        Constraints:
+//
+//        1 <= nums.length <= 3 * 104
+//        -3 * 104 <= nums[i] <= 3 * 104
+//        Each element in the array appears twice except for one element which appears only once.
 public class SingleNumber {
     public static void main(String[] args) {
-        int []arr={4,1,2,1,2};
-        int num=findTheSingleNumber(arr);
+        int[] arr = {4, 1, 2, 1, 2};
+        int num = findTheSingleNumber(arr);
         System.out.println(num);
     }
 
     private static int findTheSingleNumber(int[] arr) {
-      int ans=0;
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j < arr.length-1; j++) {
-                if(arr[i]!=arr[j]){
-                    ans=arr[i];
+        int ans = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[i] != arr[j]) {
+                    ans = arr[i];
                     break;
                 }
             }
--- arrays/Sort0and1.java
+++ arrays/Sort0and1.java
@@ -7,21 +7,24 @@ public class Sort0and1 {
     public static void main(String[] args) {
         int[] nums = {0, 1, 0, 0, 1, 1, 1, 0, 0};
         int[] ans = sortArrayTwoPointer(nums);
-        int[] sorted = sortArrayappraoch2(nums);
+        int[] sorted = sortArrayApproach2(nums);
         System.out.println(Arrays.toString(ans));
         System.out.println(Arrays.toString(sorted));
     }
 
-    private static int [] sortArrayappraoch2(int[] nums) {
+    private static int [] sortArrayApproach2(int[] nums) {
         int count=0;
+        // Count the number of 0's in the array
         for(int i=0;i< nums.length;i++){
             if(nums[i]==0){
                 count++;
             }
         }
+        // Replace the first side of array with 0 as you now know the count of 0's
         for (int i = 0; i <=count; i++) {
             nums[i]=0;
         }
+        // Replace the count to n-1 elements with 1 in the array.
         for (int i = count; i < nums.length; i++) {
             nums[i]=1;
         }
